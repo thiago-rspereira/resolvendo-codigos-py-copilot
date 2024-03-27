e7aaab2e086745b0399efcdbe2745aceabe08467
Thiago Pereira <133459464+thiago-rspereira@users.noreply.github.com>
2024-03-27 03:05:20 -0300

    Adicionado programa Python.

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,11 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+# Solicita ao usuário dois dados diferentes
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatena os dados em uma única string
+concatenado = dado1 + " " + dado2
+
+# Imprime a string resultante
+print("Dados concatenados:", concatenado)
