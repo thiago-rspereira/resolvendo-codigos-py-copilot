36e31cf8c37db309b9ade42b8e25054ba4d801e3
Thiago Pereira <133459464+thiago-rspereira@users.noreply.github.com>
2024-03-27 03:14:39 -0300

    Adiciona repetição de string

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,11 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+# Solicita uma string e um número inteiro como entrada
+texto = input("Digite a string que deseja repetir: ")
+repeticoes = int(input("Digite o número de repetições desejado: "))
+
+# Repete a string o número de vezes informado
+texto_repetido = texto * repeticoes
+
+# Imprime a string repetida
+print("Texto repetido:", texto_repetido)
